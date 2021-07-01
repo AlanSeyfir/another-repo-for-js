329aaf5e48b0beb735b2430b14b8ccd01ea5a4d6
alanseyfir <alan.garcia.nunez@gmail.com>
2021-07-01 16:40:29 -0500

    An attempt to make a chessboard

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*---------------------------------Operators---------------------------------*/
 let myParagraph = document.createElement('div');
 let myText = document.createTextNode('Dummy text to add');
 
@@ -34,7 +35,7 @@ console.group('Try-out-3');
     console.log(0 + '' + day + '-' + month + '-' + year );
 console.groupEnd();
 
-console.log(isNaN(year)); 
+console.log(isNaN('sasas')); 
 
 /*
 Write JavaScript program to convert ⁰C (Celsius) to ⁰F (Fahrenheit). 
@@ -45,12 +46,55 @@ is used to display whether a value is a number or not.
 */
 
 /* Try-it-out 4 */
-//let userAnswer = window.prompt('Celsius to Fahrenheit');
-let result = parseFloat((userAnswer * (9/5)) + 32).toFixed(1);
+// let userAnswer = window.prompt('Celsius to Fahrenheit');
+// let result = parseFloat((userAnswer * (9/5)) + 32).toFixed(1);
 
-console.group('Try-out-4');
-    console.log('User input: ' + userAnswer);
-    console.log('Celius: ' + userAnswer + '°C to ' + result + '°F');
+// console.group('Try-out-4');
+//     console.log('User input: ' + userAnswer);
+//     console.log('Celius: ' + userAnswer + '°C to ' + result + '°F');
 
-    alert('Answer in console')
-console.groupEnd();
+//     alert('Answer in console')
+// console.groupEnd();
+
+/*---------------------------------Loops and Conditions---------------------------------*/
+
+/* 
+    Write a program to draw a chessboard of size 8X8.
+    Each line of the chessboard should have a + and a - . + indicates black cell and - indicates a white cell on the chess board.
+    Draw a border with * on all four sides.
+    You can try this using different loops.
+*/
+/* Try-it-out 5 */
+let blackSpace = '+';
+let whiteSpace = '-';
+let border = '*';
+
+for(let i = 0; i < 8; i++){
+    //console.log(border);
+    if(i === 0){
+        console.log(border + ' ' + border + ' ' + border + ' ' + border + ' ' + border + ' ' + border + ' ' + border + ' ' + border + ' ');
+    }
+
+    // if(i % 2 === 0){
+    //     console.log(blackSpace);
+    // }else{
+    //     console.log(whiteSpace);
+    // }
+
+    for(let j = 0; j < 1; j++){
+        //console.log(whiteSpace);
+        if((i + j) % 2 == 0){
+            console.log('* ' + blackSpace + ' ' + whiteSpace + ' ' + blackSpace + ' ' + whiteSpace + ' ' + blackSpace + ' ' + whiteSpace + ' ' + border);
+
+            console.log('* ' + whiteSpace + ' ' + blackSpace + ' ' + whiteSpace + ' ' + blackSpace + ' ' + whiteSpace + ' ' + blackSpace + ' ' + border);
+        }
+
+        if(j >= 1){
+            console.log("dsdsdws");
+        }
+    }
+
+    if(i === 7){
+        console.log(border + ' ' + border + ' ' + border + ' ' + border + ' ' + border + ' ' + border + ' ' + border + ' ' + border + ' ');
+    }
+}
