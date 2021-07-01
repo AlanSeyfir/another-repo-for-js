0e015f718695932d3f866120c1bcb314e7753e5d
alanseyfir <alan.garcia.nunez@gmail.com>
2021-06-30 22:19:53 -0500

    3 and 4 exercise done!

--- main.js
+++ main.js
@@ -33,3 +33,24 @@ console.group('Try-out-3');
     //Print the date as 01-Jan-2016 when
     console.log(0 + '' + day + '-' + month + '-' + year );
 console.groupEnd();
+
+console.log(isNaN(year)); 
+
+/*
+Write JavaScript program to convert ⁰C (Celsius) to ⁰F (Fahrenheit). 
+You can prompt the user for input ⁰C using window.prompt. 
+Formula for converting ⁰C to ⁰F is F = (12°C × 9/5) + 32
+
+is used to display whether a value is a number or not.
+*/
+
+/* Try-it-out 4 */
+//let userAnswer = window.prompt('Celsius to Fahrenheit');
+let result = parseFloat((userAnswer * (9/5)) + 32).toFixed(1);
+
+console.group('Try-out-4');
+    console.log('User input: ' + userAnswer);
+    console.log('Celius: ' + userAnswer + '°C to ' + result + '°F');
+
+    alert('Answer in console')
+console.groupEnd();
